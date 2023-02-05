be8b993a783f5834212c55f4a57ee9e7fda12708
Nicolas Bouliane <contact@nicolasbouliane.com>
2023-02-05 19:52:04 +0100

    Clean repository, keep only content and templates

--- scripts/list-missing-images.sh
+++
@@ -1,6 +0,0 @@
-echo "Listing images linked in content..."
-in_use=$(grep -orh "\/images\/[^\"\'\) ]*" content | sort -u)
-echo "Listing images in /images..."
-all_images=$(find content/images -type f | cut -c8- | sort -u)
-
-grep -F -x -v -f <(echo "$all_images") <(echo "$in_use")
