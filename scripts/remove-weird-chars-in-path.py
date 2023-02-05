2fb9dab878d23d2d9a2d2db0e3fc6e7a5978c62e
Nicolas Bouliane <contact@nicolasbouliane.com>
2023-02-05 20:00:55 +0100

    Moved scripts from aab-server

---
+++ scripts/remove-weird-chars-in-path.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+
+weird_chars = (
+    ('\uFEFF', ''),
+    ('\u200B', ''),
+    ('ü', 'ü'),
+)
+
+for char, replacement in weird_chars:
+    paths_with_bom = list(Path('.').rglob(f'*{char}*'))
+    for old_path in paths_with_bom:
+        new_path = Path(str(old_path).replace(char, replacement))
+        print(f"Fixing {repr(str(old_path))}\n\t -> {repr(str(new_path))}")
+        old_path.rename(new_path)
