89c3b0c2c51229efacc4e78ec85bb491b92cbf83
Nicolas Bouliane <contact@nicolasbouliane.com>
2023-02-09 22:39:45 +0100

    Rebuild ursus when it changes

--- ursus_builder/watch-for-updates.sh
+++ ursus_builder/watch-for-updates.sh
@@ -2,26 +2,52 @@
 # Pulls the latest version of All About Berlin and rebuilds it as needed
 set -e
 
-if [ ! -d /var/ursus/site/.git ]
+URSUS_PATH=/usr/lib/ursus
+URSUS_REPO_URL="https://github.com/nicbou/ursus.git"
+alias ursus_git="git -C $URSUS_PATH"
+alias install_ursus="pip install -e $URSUS_PATH"
+
+SITE_PATH=/var/ursus/site
+SITE_REPO_URL=$GIT_REPO_URL
+alias site_git="git -C $SITE_PATH"
+alias build_site="ursus -c $SITE_PATH/ursus_config.py"
+
+# Clone repos if they don't exist
+if [ ! -d "$URSUS_PATH/.git" ]
 then
-    git clone ${GIT_REPO_URL} /var/ursus/site
-    ursus -c /var/ursus/site/ursus_config.py;
+    mkdir -p $URSUS_PATH
+    git clone $URSUS_REPO_URL $URSUS_PATH
+fi
+install_ursus
+
+if [ ! -d "$SITE_PATH/.git" ]
+then
+    mkdir -p $SITE_PATH
+    git clone $SITE_REPO_URL $SITE_PATH
+    build_site;
 fi
 
+# Initial build on start
+site_git pull;
+build_site;
 
-# Build once
-git -C /var/ursus/site pull;
-ursus -c /var/ursus/site/ursus_config.py
-
-
-# Rebuild on changes
+# Wait for changes
 while true; do
-    git -C /var/ursus/site fetch > /dev/null
-    if ! (git -C /var/ursus/site diff --exit-code master..origin/master > /dev/null)
-    then
-        git -C /var/ursus/site pull;
-        ursus -c /var/ursus/site/ursus_config.py;
-    else
-        sleep 60
+    # Update ursus if needed
+    ursus_git fetch > /dev/null
+    ursus_commit_count=$(ursus_git rev-list HEAD...origin/master --count)
+    if [ "$ursus_commit_count" -gt "0" ]; then
+        ursus_git pull
+        install_ursus
     fi
+
+    # Rebuild site if needed
+    site_git fetch > /dev/null
+    site_commit_count=$(site_git rev-list HEAD...origin/master --count)
+    if [ "$site_commit_count" -gt "0" ]; then
+        site_git pull
+        build_site
+    fi
+
+    sleep 60
 done
