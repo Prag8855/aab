7fc6d428b208c9c7829f06284cc14e8312df9b60
Nicolas Bouliane <contact@nicolasbouliane.com>
2023-06-27 15:55:23 +0200

    Improved health insurance suggestions, embedded health insurance question

--- templates/_js/vue/health-insurance-question.js
+++ templates/_js/vue/health-insurance-question.js
@@ -1,19 +1,70 @@
+{% include '_js/constants.js' %}
 {% include '_js/utils.js' %}
 {% include '_js/vue.js' %}
-{% include '_js/vue/date-picker.js' %}
+{% include '_js/vue/eur.js' %}
 {% js %}{% raw %}
 Vue.component('health-insurance-question', {
+  props: {
+    occupation: String,
+    age: Number,
+    income: Number,
+    isMarried: Boolean,
+    hasChildren: Boolean,
+  },
   data: function() {
     return {
-      dateOfBirth: '',
-      emailAddress: '',
-      fullName: '',
-      incomeOverLimit: false,
-      occupation: 'employee',
-      phoneNumber: '',
-      question: '',
+      uniqueId: Math.floor(Math.random() * 10000),
       stage: 'contactInfo',
+      minFreiwilligMonthlyIncome: healthInsurance.minFreiwilligMonthlyIncome * 12,
+
+      question: '',
+      fullName: '',
+      emailAddress: '',
+      phoneNumber: '',
+
+      inputAge: this.age || getDefaultNumber('age', ''),
+      inputOccupation: this.occupation || getDefault('occupation', 'employee'),
+      incomeOverLimit: this.income >= (healthInsurance.minFreiwilligMonthlyIncome * 12),
     };
+  },
+  created(){
+    this.question = '';
+
+    if(this.age && this.occupation){
+      const formattedIncome = `${formatCurrency(this.income, false, '€', false)} per year`;
+      this.question += {
+        employee: `I am an employee, and I make ${formattedIncome}.`,
+        azubi: `I am an apprentice, and I make ${formattedIncome}.`,
+        studentEmployee: `I am a working student, and I make ${formattedIncome}.`,
+        studentSelfEmployed: `I am a self-employed student, and I make ${formattedIncome}.`,
+        student: `I am a student, `,
+        selfEmployed: `I am self-employed, and I make ${formattedIncome}.`,
+        unemployed: `I am unemployed.`,
+      }[this.occupation];
+    }
+
+    if (this.age) {
+      this.question += ` I am ${this.age} years old,`;
+    }
+
+    if(this.isMarried){
+      if(this.hasChildren){
+        this.question += " I am married and I have children.";
+      }
+      else {
+        this.question += " I am married and I don't have children.";
+      }
+    }
+    else{
+      if(this.hasChildren){
+        this.question += " and I am not married, but I have children.";
+      }
+      else {
+        this.question += " I have children and I am not married.";
+      }
+    }
+
+    this.question +=  ' Which health insurance should I choose?';
   },
   methods: {
     setStage(stage) {
@@ -32,9 +83,9 @@ Vue.component('health-insurance-question', {
               name: this.fullName,
               email: this.emailAddress,
               phone: this.phoneNumber || '',
-              income: this.incomeOverLimit ? 'Over {{ GKV_FREIWILLIG_VERSICHERT_MIN_INCOME | currency }}€/year' : 'Under {{ GKV_FREIWILLIG_VERSICHERT_MIN_INCOME | currency }}€/year',
-              occupation: this.occupation,
-              dateOfBirth: this.dateOfBirth,
+              income: this.incomeOverLimit ? `Over ${this.minFreiwilligMonthlyIncome}€/year` : `Under ${this.minFreiwilligMonthlyIncome}€/year`,
+              occupation: this.inputOccupation,
+              dateOfBirth: `${this.inputAge} years old`,
               question: this.question,
             }),
           }
@@ -54,9 +105,12 @@ Vue.component('health-insurance-question', {
         this.setStage('thank-you');
       }
     },
+    makeId(id){
+      return `${id}-${this.uniqueId}`;
+    }
   },
   template: `
-    <div ref="form">
+    <div ref="form" class="health-insurance-question">
       <template v-if="stage === 'contactInfo'">
         <div class="form-recipient">
           <img
@@ -67,76 +121,79 @@ Vue.component('health-insurance-question', {
         </div>
         <hr>
         <div class="form-group">
-          <label for="question-{% endraw %}{{ id }}{% raw %}">Your question</label>
+          <label :for="makeId('question')">Your question</label>
           <div class="input-group">
-            <textarea v-model="question" id="question-{% endraw %}{{ id }}{% raw %}" required placeholder=" "></textarea>
+            <textarea v-model="question" :id="makeId('question')" required placeholder=" "></textarea>
             <span class="input-symbols"></span>
+            <span class="input-instructions">If you are applying for a <glossary term="Aufenthaltstitel">residence permit</glossary>, mention it.</span>
           </div>
         </div>
         <hr>
-        <div class="form-group">
+        <div class="form-group" v-if="!occupation">
           <span class="label">Your occupation</span>
-          <select v-model="occupation">
+          <select v-model="inputOccupation">
             <option value="employee">Employee</option>
             <option value="selfEmployed">Self-employed</option>
             <option value="student">Student</option>
             <option value="other">Other</option>
           </select>
         </div>
-        <div class="form-group">
+        <div class="form-group" v-if="!income">
           <span class="label"></span>
           <div class="input-group">
             <label class="checkbox">
-              <input type="checkbox" v-model="incomeOverLimit"> I earn more than {% endraw %}{{ GKV_FREIWILLIG_VERSICHERT_MIN_INCOME | currency }}{% raw %}€ per year
+              <input type="checkbox" v-model="incomeOverLimit"> I earn more than <eur :amount="minFreiwilligMonthlyIncome"></eur> per year
             </label>
           </div>
         </div>
-        <hr>
+        <hr v-if="!income && !occupation">
         <div class="form-group">
-          <label for="name-{% endraw %}{{ id }}{% raw %}">
+          <label :for="makeId('name')">
             Name
           </label>
           <div class="input-group">
-            <input v-model="fullName" type="text" id="name-{% endraw %}{{ id }}{% raw %}" required autocomplete="name">
+            <input v-model="fullName" type="text" :id="makeId('name')" required autocomplete="name">
             <span class="input-symbols"></span>
             {% endraw %}{% include "_blocks/formHoneypot.html" %}{% raw %}
           </div>
         </div>
         <div class="form-group">
-          <label for="email-{% endraw %}{{ id }}{% raw %}">
+          <label :for="makeId('email')">
             Email
           </label>
           <div class="input-group">
-            <input v-model="emailAddress" type="email" id="email-{% endraw %}{{ id }}{% raw %}" required autocomplete="email">
+            <input v-model="emailAddress" type="email" :id="makeId('email')" required autocomplete="email">
             <span class="input-symbols"></span>
           </div>
         </div>
         <div class="form-group">
-          <label for="phone-{% endraw %}{{ id }}{% raw %}">
+          <label :for="makeId('phone')">
             Phone number
           </label>
           <div class="input-group">
-            <input v-model="phoneNumber" type="tel" id="phone-{% endraw %}{{ id }}{% raw %}" placeholder="+49..." autocomplete="tel" aria-describedby="instructions-phone-{% endraw %}{{ id }}{% raw %}">
-            <span class="input-instructions" id="instructions-phone-{% endraw %}{{ id }}{% raw %}">Only if you prefer a phone call.</span>
+            <input v-model="phoneNumber" type="tel" :id="makeId('phone')" placeholder="+49..." autocomplete="tel" :aria-describedby="makeId('instructions-phone')">
+            <span class="input-instructions" :id="makeId('instructions-phone')">Only if you prefer a phone call.</span>
           </div>
         </div>
         <hr>
-        <div class="form-group">
-          <label for="date-of-birth-{% endraw %}{{ id }}{% raw %}-day">
-            Date of birth
+        <div class="form-group" v-if="!age">
+          <label :for="makeId('age')">
+            Age
           </label>
-          <div class="input-group">
-            <date-picker v-model="dateOfBirth" id="date-of-birth-{% endraw %}{{ id }}{% raw %}" required aria-describedby="instructions-age-{% endraw %}{{ id }}{% raw %}"></date-picker>
-            <span class="input-symbols"></span>
-            <span class="input-instructions" id="instructions-age-{% endraw %}{{ id }}{% raw %}">Your age affects your health insurance options.</span>
-          </div>
+          <label class="input-group">
+            <age-input v-model="inputAge" :id="makeId('age')" required :aria-describedby="makeId('instructions-age')"></age-input>
+            years old
+            <span class="input-instructions" :id="makeId('instructions-age')">Your age affects your health insurance options.</span>
+          </label>
         </div>
         <div class="buttons">
-          <button class="button primary no-print" @click="submitForm">Ask Rob <i class="icon right"></i></button>
+          <slot name="form-buttons"></slot>
+          <button class="button primary no-print" @click="submitForm">Send question</button>
         </div>
       </template>
       <template v-if="stage === 'thank-you'">
-        <p><strong>Message sent!</strong> Rob will contact you today or during the next business day. Expect an email from Feather.</p>
+        <p><strong>Message sent!</strong> Rob will contact you today or during the next business day. Expect an email from Feather Insurance.</p>
+        <slot name="after-confirmation"></slot>
       </template>
     </div>
   `,
