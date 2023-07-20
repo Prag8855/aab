535bd9c307a76896d77355409f6910e66670ea0c
Nicolas Bouliane <contact@nicolasbouliane.com>
2023-07-20 13:47:48 +0200

    Treat whitespace as blank string for <blank> placeholder

--- templates/_js/vue/blank.js
+++ templates/_js/vue/blank.js
@@ -4,6 +4,6 @@ Vue.component('blank', {
 	props: {
 		placeholder: String,
 	},
-	template: `<output :class="{'placeholder': !$slots.default[0].text}" v-html="$slots.default[0].text || placeholder"></output>`,
+	template: `<output :class="{'placeholder': !$slots.default[0].text.trim()}" v-html="$slots.default[0].text.trim() || placeholder"></output>`,
 });
 {% endraw %}{% endjs %}
