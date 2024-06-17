2d71152d716696c7dc3211da9ef13262ae88735c
Nicolas Bouliane <contact@nicolasbouliane.com>
2024-06-17 16:23:22 +0200

    Improve footnotes linter

--- extensions/linters/footnotes.py
+++ extensions/linters/footnotes.py
@@ -20,7 +20,7 @@ class CitationNeededLinter(RegexLinter):
     Raises error on "Citation needed"
     """
     file_suffixes = '.md'
-    regex = re.compile(r'.{0,5}[Cc]itation needed.{0,5}')
+    regex = re.compile(r'.{0,5}[Cc]itation needed.*')
 
     def handle_match(self, file_path: Path, match: re.Match):
         yield f"Missing source: {match.group(0)}", logging.ERROR
