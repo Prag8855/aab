2f6700a24ea28b187888e37698f88fc4cae63a8c
Nicolas Bouliane <contact@nicolasbouliane.com>
2024-09-02 13:26:47 +0200

    resignationLetter: Extract name and salutations formatting to generic functions

---
+++ templates/_js/letterUtils.js
@@ -0,0 +1,42 @@
+{% js %}
+function formatName(gender, firstName, lastName, language='en'){
+	let displayGender = {
+		man: { en: 'Mr', de: 'Herr' },
+		woman: { en: 'Mrs', de: 'Frau' },
+		other: { en: '', de: '' },
+	}[gender][language];
+
+	if(!lastName){ // No "Dear Mr Firstname"
+		displayGender = '';
+	}
+
+	if(displayGender && lastName){
+		return [displayGender, lastName].join(' '); 
+	}
+
+	return [displayGender, firstName, lastName].filter(Boolean).join(' ');
+}
+
+function formatSalutations(gender, firstName, lastName, language='en'){
+	if(!lastName || (gender === 'other' && !firstName)) {
+		return {
+			en: 'Dear Sir or Madam',
+			de: 'Sehr geehrte Damen und Herren'
+		}[language];
+	}
+	return {
+		'man': {
+			en: `Dear Mr ${lastName}`,
+			de: `Sehr geehrter Herr ${lastName}`,
+		},
+		'woman': {
+			en: `Dear Mrs ${lastName}`,
+			de: `Sehr geehrte Frau ${lastName}`,
+		},
+		'other': {
+			en: `Dear ${firstName} ${lastName}`,
+			de: `Sehr geehrte*r ${firstName} ${lastName}`,
+		},
+	}[gender][language];
+}
+{% endjs %}
