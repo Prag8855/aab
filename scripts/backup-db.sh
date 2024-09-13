7c92bec85c8f232a33ba3aed7b02229179a27932
Nicolas Bouliane <contact@nicolasbouliane.com>
2024-09-13 19:12:32 +0200

    backup-db.sh: Remove unused script

--- scripts/backup-db.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source .env
-set -e
-
-DB_CONTAINER=`docker-compose ps -q api`
-
-echo "Copying database to $1"
-docker cp "${DB_CONTAINER}:/var/db/api.db" $1
-echo "Database backup done."
