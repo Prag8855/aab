c64a862b53cc67cf1ba9dac96b146f8e621061ff
Nicolas Bouliane <contact@nicolasbouliane.com>
2024-10-23 12:10:57 +0200

    Rewrite: Set up cron jobs

--- api/start.sh
+++ api/start.sh
@@ -5,7 +5,16 @@ set -x
 python3 manage.py makemigrations
 python3 manage.py migrate --noinput
 
+# Capture cron logs
+rm -f /tmp/stdout /tmp/stderr
+mkfifo /tmp/stdout /tmp/stderr
+chmod 0666 /tmp/stdout /tmp/stderr
+tail -f /tmp/stdout &
+tail -f /tmp/stderr >&2 &
+
+# Start cron and pass environment variables to it
+printenv > /etc/environment
 crontab /srv/crontab.conf
-cron -L /proc/1/fd/1
+service cron start
 
 gunicorn --reload --config /srv/gunicorn_config.py api.wsgi:application
