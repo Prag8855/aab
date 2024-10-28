ab69623462386f0342d281442b62f08d772f85a3
Nicolas Bouliane <contact@nicolasbouliane.com>
2024-10-28 14:47:06 +0100

    Extend localStorageMixin to more properties

--- templates/_js/vue/localStorageMixin.js
+++ templates/_js/vue/localStorageMixin.js
@@ -12,14 +12,39 @@ const localStorageMixin = {
 			'nationality',
 			'occupation',
 			'state',
-		])
+		]);
+
+		const desiredNumberKeys = new Set([
+			'age',
+			'childrenCount',
+			'yearlyIncome',
+		]);
+
+		const desiredBooleanKeys = new Set([
+			'isMarried',
+			'useMonthlyIncome',
+		]);
 
 		availableKeys.intersection(desiredStringKeys).forEach(key => {
 			this[key] = getDefault(key, this.key);
 			this.$watch(key, function (newVal) {
 				setDefault(key, newVal);
 			})
-		})
+		});
+
+		availableKeys.intersection(desiredNumberKeys).forEach(key => {
+			this[key] = getDefaultNumber(key, this.key);
+			this.$watch(key, function (newVal) {
+				setDefaultNumber(key, newVal);
+			})
+		});
+
+		availableKeys.intersection(desiredBooleanKeys).forEach(key => {
+			this[key] = getDefaultBoolean(key, this.key);
+			this.$watch(key, function (newVal) {
+				setDefaultBoolean(key, newVal);
+			})
+		});
 	},
 }
 {% endjs %}
