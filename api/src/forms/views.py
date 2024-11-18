870fdfc4194aee78f33966ff43846852183acea9
Nicolas Bouliane <contact@nicolasbouliane.com>
2024-11-18 21:48:58 +0900

    Create /forms/residence-permit-feedback/summary endpoint

--- api/src/forms/views.py
+++ api/src/forms/views.py
@@ -1,11 +1,14 @@
 from django.core.exceptions import ValidationError as DjangoValidationError
+from django.db import connection
 from forms.models import HealthInsuranceQuestion, PensionRefundQuestion, PensionRefundReminder, PensionRefundRequest, \
     ResidencePermitFeedback, TaxIdRequestFeedbackReminder
 from forms.serializers import HealthInsuranceQuestionSerializer, \
     PensionRefundQuestionSerializer, PensionRefundReminderSerializer, PensionRefundRequestSerializer, \
     PublicResidencePermitFeedbackSerializer, ResidencePermitFeedbackSerializer, TaxIdRequestFeedbackReminderSerializer
 from rest_framework import mixins, permissions, viewsets
+from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError as DRFValidationError
+from rest_framework.response import Response
 from rest_framework.serializers import as_serializer_error
 from rest_framework.views import exception_handler as drf_exception_handler
 
@@ -72,6 +75,75 @@ class ResidencePermitFeedbackViewSet(FeedbackViewSet):
             if param in self.request.query_params
         }
         return self.queryset.filter(**filters)
+
+    @action(detail=False, methods=['get'])
+    def summary(self, request, format=None):
+        def get_date_range(cursor, date_column_start, date_column_end, residence_permit_type, department):
+            residence_permit_type_filter = "AND residence_permit_type=%(residence_permit_type)s" if residence_permit_type else ''
+            department_filter = "AND department=%(department)s" if department else ''
+            percentiles_query = f"""
+                WITH time_diffs AS (
+                    SELECT
+                        CAST( (julianday({date_column_end}) - julianday({date_column_start})) AS INT) AS time_diff
+                    FROM forms_residencepermitfeedback
+                    WHERE
+                        {date_column_end} IS NOT NULL
+                        AND {date_column_start} IS NOT NULL
+                        {residence_permit_type_filter}
+                        {department_filter}
+                    ORDER BY time_diff ASC
+                ),
+                total_rows AS (
+                    SELECT COUNT(*) AS total_rows FROM time_diffs
+                ),
+                percentile_rows AS(
+                    SELECT
+                        CAST((total_rows * 0.2) AS INT) AS row_20,
+                        CAST((total_rows * 0.8) AS INT) AS row_80
+                    FROM total_rows
+                ),
+                numbered_rows AS (
+                    SELECT
+                        time_diff,
+                        row_20,
+                        row_80,
+                        ROW_NUMBER() over (order by time_diff) as rownum
+                    FROM time_diffs, percentile_rows
+                )
+                SELECT time_diff
+                FROM numbered_rows
+                WHERE rownum IN (row_20, row_80)
+            """
+            cursor.execute(percentiles_query, {
+                'residence_permit_type': residence_permit_type,
+                'department': department,
+            })
+            percentile_20, percentile_80 = [row[0] for row in cursor.fetchall()]
+            return percentile_20, percentile_80
+
+        with connection.cursor() as cursor:
+            residence_permit_type = self.request.query_params.get('residence_permit_type')
+            department = self.request.query_params.get('department')
+            response_data = {
+                'steps': {
+                    'response_range': get_date_range(
+                        cursor,
+                        'application_date', 'first_response_date',
+                        residence_permit_type, department
+                    ),
+                    'appointment_range': get_date_range(
+                        cursor,
+                        'first_response_date', 'appointment_date',
+                        residence_permit_type, department
+                    ),
+                    'pick_up_range': get_date_range(
+                        cursor,
+                        'appointment_date', 'pick_up_date',
+                        residence_permit_type, department
+                    ),
+                },
+            }
+        return Response(response_data, status=200)
 
 
 class TaxIdRequestFeedbackReminderViewSet(MessageViewSet):
