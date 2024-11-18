991db8b924edfe2dc43c9005ba6aae3794fa9c7e
Nicolas Bouliane <contact@nicolasbouliane.com>
2024-11-18 21:48:35 +0900

    Default to JSON responses

--- api/src/api/settings.py
+++ api/src/api/settings.py
@@ -93,6 +93,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
     'EXCEPTION_HANDLER': 'forms.views.exception_handler',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
