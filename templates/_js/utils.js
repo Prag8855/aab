14bec03e703730dd48e5a1b6b6c67707aa618ce7
Nicolas Bouliane <contact@nicolasbouliane.com>
2025-06-26 16:12:09 +0200

    taxCalculator: Show monthly health insurance cost when useMonthlyIncome is true

--- templates/_js/utils.js
+++ templates/_js/utils.js
@@ -1,8 +1,8 @@
 {% js %}
-function formatPercent(num, addSymbol=true) {
+function formatPercent(num, addSymbol=true, maxDecimals=3) {
 	const formattedNum = num.toLocaleString('en-GB', {
 		minimumFractionDigits: 0,
-		maximumFractionDigits: 3,
+		maximumFractionDigits: maxDecimals,
 	});
 	return addSymbol ? `${formattedNum}%` : formattedNum;
 }
