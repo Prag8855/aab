dc34db38bc0f78a9dd1e7447e4449dd1272f24fd
Nicolas Bouliane <contact@nicolasbouliane.com>
2025-08-10 13:14:43 +0200

    blank: Add required attribute and style for <blank> tags

--- templates/_js/vue/blank.js
+++ templates/_js/vue/blank.js
@@ -3,12 +3,13 @@
 Vue.component('blank', {
 	props: {
 		placeholder: String,
+		required: Boolean,  // Show in red if empty
 	},
 	computed: {
 		content() {
 			return this.$slots && this.$slots.default && this.$slots.default[0].text.trim();
 		}
 	},
-	template: `<output :class="{'placeholder': !content}" v-html="content || placeholder"></output>`,
+	template: `<output :class="{placeholder: !content, error: required && !content}" v-html="content || placeholder"></output>`,
 });
 {% endraw %}{% endjs %}
