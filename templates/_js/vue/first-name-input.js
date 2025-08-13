dbb8e1e3d521976b9b1e9e0dca21f11386dbc550
Nicolas Bouliane <contact@nicolasbouliane.com>
2025-08-13 11:55:54 +0200

    first-name-input, last-name-input: Use simple placeholder for everyone

--- templates/_js/vue/first-name-input.js
+++ templates/_js/vue/first-name-input.js
@@ -2,30 +2,12 @@
 {% js %}{% raw %}
 Vue.component('first-name-input', {
 	props: ['value'],
-	data(){
-		const mainLanguage = (navigator.languages || []).map(lang => lang.substring(0, 2))[0];
-		console.log((navigator.languages || []).map(lang => lang.substring(0, 2)))
-		const placeholder = {
-			ar: 'Noor',
-			bn: 'Kiran',
-			fr: 'Maxime',
-			hi: 'Aadi',
-			it: 'Andrea',
-			ja: 'Masami',
-			nl: 'Sam',
-			pl: 'Jan',
-			ru: 'Sasha',
-			tr: 'Eren',
-			uk: 'Zhenya',
-		}[mainLanguage] || 'Alex';
-		return { placeholder }
-	},
 	template: `
 		<input type="text"
 			class="first-name-input"
 			:value="value"
 			v-on:input="$emit('input', $event.target.value)"
-			:placeholder="placeholder"
+			placeholder="Alex"
 			autocomplete="given-name"
 			title="First name">
 	`,
