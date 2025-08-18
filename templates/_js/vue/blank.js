e1b9d1ea117d1a22115e5fc096f7ab883579b117
Nicolas Bouliane <contact@nicolasbouliane.com>
2025-08-18 18:42:27 +0200

    blank: Remove required attribute

--- templates/_js/vue/blank.js
+++ templates/_js/vue/blank.js
@@ -3,13 +3,12 @@
 Vue.component('blank', {
 	props: {
 		placeholder: String,
-		required: Boolean,  // Show in red if empty
 	},
 	computed: {
 		content() {
 			return this.$slots && this.$slots.default && this.$slots.default[0].text.trim();
 		}
 	},
-	template: `<output :class="{placeholder: !content, error: required && !content}" :title="required && !content ? 'This information is required' : ''" v-html="content || placeholder"></output>`,
+	template: `<output :class="{placeholder: !content}" :title="content ? '' : 'This information is required'" v-html="content || placeholder"></output>`,
 });
 {% endraw %}{% endjs %}
