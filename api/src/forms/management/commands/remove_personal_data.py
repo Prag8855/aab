e52ec1e247e6e87143e4414afd56771f9e799d20
Nicolas Bouliane <contact@nicolasbouliane.com>
2025-08-19 06:15:43 +0200

    remove_personal_data: Dynamically retrieve list of ScheduledMessage classes

--- api/src/forms/management/commands/remove_personal_data.py
+++ api/src/forms/management/commands/remove_personal_data.py
@@ -1,8 +1,9 @@
 from datetime import timedelta
+from django.apps import apps
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils import timezone
-from forms.models import MessageStatus, scheduled_message_models
+from forms.models import MessageStatus, ScheduledMessage
 import logging
 import requests
 
@@ -17,6 +18,9 @@ class Command(BaseCommand):
         ninety_days_ago = timezone.now() - timedelta(days=90)
 
         count = 0
+
+        scheduled_message_models = [model for model in apps.get_models() if issubclass(model, ScheduledMessage)]
+
         for model in scheduled_message_models:
             scheduled_messages = model.objects.filter(
                 status=MessageStatus.SENT,
