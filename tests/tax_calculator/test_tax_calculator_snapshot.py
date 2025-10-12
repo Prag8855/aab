e1b8326fbf1440ff503ea890d49b6f741602f43f
Nicolas Bouliane <contact@nicolasbouliane.com>
2025-10-12 15:19:03 +0200

    tests: Create specific test page for tax calculator, update snapshots

--- tests/tax_calculator/test_tax_calculator_snapshot.py
+++ tests/tax_calculator/test_tax_calculator_snapshot.py
@@ -2,7 +2,7 @@ from . import fill_calculator
 
 
 def test_snapshot(page, assert_snapshot):
-    page.goto("/tools/tax-calculator")
+    page.goto("/tests/component/tax-calculator")
     calculator = page.get_by_role("group", name="German tax calculator")
     assert_snapshot(calculator.screenshot())
     page.get_by_role("link", name="Show options").click()
