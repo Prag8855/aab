5c58987f8afbebd1a9e3c85155ad7a8e244bff37
Nicolas Bouliane <contact@nicolasbouliane.com>
2025-10-17 12:43:04 +0200

    tests: Test <glossary> in separate file, add glossary open and close test

--- tests/components/test_components.py
+++ tests/components/test_components.py
@@ -6,9 +6,22 @@ import pytest
     [
         "collapsible",
         "eur",
+        "glossary",
     ],
 )
 def test_component_snapshot(page, assert_snapshot, component):
     page.goto(f"/tests/component/{component}")
     content = page.locator("main > article")
     assert_snapshot(content.screenshot())
+
+
+def test_glossary_click(page, assert_snapshot):
+    page.goto("/tests/component/glossary")
+    page.locator("main > article a").first.click()
+
+    dialog = page.locator("dialog").first
+    assert dialog.evaluate("dialog => dialog.open")
+    assert_snapshot(dialog.screenshot())
+
+    dialog.locator(".close-button").click()
+    assert not dialog.evaluate("dialog => dialog.open")
