d6cf878285a17c1b7934b2447d49125e81a704ce
Nicolas Bouliane <contact@nicolasbouliane.com>
2025-10-21 10:29:35 +0200

    .prod/deploy: Enable logging, fail on bad pull

--- .prod/deploy.sh
+++ .prod/deploy.sh
@@ -2,11 +2,16 @@
 set -e
 set -x
 
+mkdir -p /var/log/allaboutberlin
+exec >> "/var/log/allaboutberlin/deploy.log" 2>&1
+
 PROJECT_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
+
+cd "$PROJECT_ROOT"
 
 echo "Pulling new changes"
 git reset --hard && \
-git pull origin master && \
+git pull origin master
 
 echo "Rebuilding project"
 docker-compose up --build -d
