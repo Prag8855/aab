1402e471819ca875bbbdb023d3f89b43643ee93a
Nicolas Bouliane <contact@nicolasbouliane.com>
2025-11-25 17:12:57 +0100

    appointmentFinder: Redesign; show appointments as a calendar instead of a list

--- frontend/templates/_js/utils/date.js
+++ frontend/templates/_js/utils/date.js
@@ -14,4 +14,16 @@ function formatLongDate(date, includeSameYear=false){
 	}
 	return '';
 }
+
+function isoDay(date){
+	const yyyy = date.getFullYear();
+	const mm = String(date.getMonth() + 1).padStart(2, '0');
+	const dd = String(date.getDate()).padStart(2, '0');
+
+	return `${yyyy}-${mm}-${dd}`;
+}
+
+function isoMonth(date){
+	return isoDay(date).slice(0, 7);
+}
 {% endjs %}
