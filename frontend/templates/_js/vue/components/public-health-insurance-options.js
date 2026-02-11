25514079044905679453e4ddc0bdcafc86a4c266
Tobias Messner <tobias.d.messner@gmail.com>
2026-02-11 13:50:14 +0100

    Fix repeating sentence in health insurance calculator

--- frontend/templates/_js/vue/components/public-health-insurance-options.js
+++ frontend/templates/_js/vue/components/public-health-insurance-options.js
@@ -108,7 +108,7 @@ Vue.component('public-health-insurance-options', {
 						{% endraw %}{% include "_css/icons/health-insurance/logo-barmer.svg" %}{% raw %}
 						<div>
 							<h3 v-text="option('barmer').name"></h3>
-							<p>The second biggest insurer. They also speak English. They speak English.</p>
+							<p>The second biggest insurer. They also speak English.</p>
 						</div>
 						<price :amount="option('barmer').total.personalContribution" per-month></price>
 					</a>
